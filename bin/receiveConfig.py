f836275b4f762b89c51d6147595d34c0390b4042
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-08-31 13:54:45 +0200

    testing new function for config

--- bin/receiveConfig.py
+++ bin/receiveConfig.py
@@ -1,6 +1,7 @@
 import ast
 import pika
 from Config import *
+configdict={}
 def receiveConfig() : 
     credentials = pika.PlainCredentials('guest', 'guest')
     connection = pika.BlockingConnection(pika.ConnectionParameters('10.1.0.56',5672, '/', credentials))
@@ -12,5 +13,7 @@ def receiveConfig() :
 def callback(channel, method, properties, body) :
     configdict= ast.literal_eval(body)
     print configdict 
+def getConfig():
+    return configdict
 
 receiveConfig()
