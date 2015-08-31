75b9751915109330a4f99274ae14ad49d4190393
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-08-31 14:05:32 +0200

    added new code to fetch config

--- bin/receiveConfig.py
+++ bin/receiveConfig.py
@@ -13,12 +13,25 @@ def receiveConfig() :
 def callback(channel, method, properties, body) :
     configdict= ast.literal_eval(body)
     setConfigDict(configdict)
-def getConfig():
+
+    
+
+def createConfigObject() : 
+    config=Config()
+    configelements=getConfigDict()
+    
+    for key,value in configelements.iteritems() : 
+        if ("queue") in key : 
+            config.set_quename(value)
+        else :
+            config.set_interval(value)
+    print "quename", config.get_quename()
+    print "interval", config.get_interval()
+    
+	
+def getConfigDict():
     return configdict
 def setConfigDict(config) : 
-	configdict=config
-def createConfigObject() : 
-	config=Config()
-	
+    configdict=config
 receiveConfig()
-
+createConfigObject()
