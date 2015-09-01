9cdc85410caf69d34899b50cc73c0dd8e2fb33f0
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-09-01 20:59:38 +0200

    test logic

--- manager/manager.py
+++ manager/manager.py
@@ -1,3 +1,14 @@
+from subclasses import Queue
+from subclasses import Config
+import pika
+import couchdb
+
 class Manager():
-    def main(self):
+    def fetchConfig(self):
+        queue=Queue()
+        config=Config()
+        config.initDbConfig()
+        
+        
+        
          
