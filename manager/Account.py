0b5489a2c57b42ec601534bc025929071fa7ea6e
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-09-01 21:14:53 +0200

    new

---
+++ manager/Account.py
@@ -0,0 +1,37 @@
+'''
+Created on 1. sep. 2015
+
+@author: stianstrom
+'''
+
+class Account():
+    '''
+    classdocs
+    '''
+    course=""
+    group=""
+    
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+
+    def get_course(self):
+        return self.__course
+
+
+    def get_group(self):
+        return self.__group
+
+
+    def set_course(self, value):
+        self.__course = value
+
+
+    def set_group(self, value):
+        self.__group = value
+
+
+        
