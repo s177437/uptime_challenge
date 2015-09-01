5bb1fbfeacadd1f3328dde5be748cbd46c7c31de
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-09-01 11:58:57 +0200

    more space

--- manager/subclasses/config.py
+++ manager/subclasses/config.py
@@ -20,9 +20,11 @@ class Config:
     def convertJSONToDictionary(self,data):
         jsondict=json.loads(data)[0]
         return jsondict
+    
     def writeConfig(self, configdata):
         for key, value in configdata.iteritems() :
             self.writeToFile(key,str(value)) 
+    
     def writeToFile(self, key,value):
         file=open('config.cfg','a')
         file.write(key+ " = "+value+"\n" )
