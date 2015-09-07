ea9c509a727c0c11d928b83154a1cbbabb1af537
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-09-07 14:42:25 +0200

    added functions to create a reportq

--- manager/Report.py
+++ manager/Report.py
@@ -3,13 +3,25 @@ Created on 1. sep. 2015
 
 @author: stianstrom
 '''
-from queue import *
+from Queue import *
 class Report():
     queue = Queue()
-    
-    
+
+    groupname=""
+    course=""
     def createReportQueue(self, queuename,content):
         self.queue.createQueue(queuename, content)
+
+    def buildReport(self, content):
+        return content
+
+    def writeReportToDatabase(self,content):
+        self.createReportQueue("dbreportq", content)
+
+
+
+
+
     
     
     
