ae776577adfbc5c4f0f9c300a39792474fe38499
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-09-15 14:50:08 +0200

    new content

--- manager/manager.py
+++ manager/manager.py
@@ -8,7 +8,7 @@ class Manager():
     interpreterServer=Pyro4.Proxy("PYRONAME:interpreter")
     def fetchConfig(self):
         queue=Queue()
-        worklist=["returnTrue","returnTrue", "returnTrue","returnTrue", "returnTrue", "returnTrue"]
+        worklist=["python /root/uptime_challenge_master/testscript/check_http.py db.no","python /root/uptime_challenge_master/testscript/check_http.py vg.no", "python /root/uptime_challenge_master/testscript/check_http.py facebook.com","python /root/uptime_challenge_master/testscript/check_http.py arngren.net", "python /root/uptime_challenge_master/testscript/check_http.py db.no","python /root/uptime_challenge_master/testscript/check_http.py db.no"]
         config=Config()
         newconfig=config.initDbConfig()
         userinfo=config.requestUserCreation(newconfig)
