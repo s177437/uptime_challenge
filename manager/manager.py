fa2a681864ff0e548180b73088f68bb14925fd10
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-09-15 18:17:44 +0200

    test for group

--- manager/manager.py
+++ manager/manager.py
@@ -11,9 +11,10 @@ class Manager():
         path="/root/uptime_challenge_master/testscript/"
         worklist=["python "+path+ "check_http.py db.no","python "+path+ "check_http.py vg.no", "python "+path+ "check_http.py facebook.com","python "+path+ "check_http.py arngren.net", "python "+path+ "check_http.py db.no","python "+path+ "check_http.py db.no"]
         config=Config()
+        config.getAccount()
         newconfig=config.initDbConfig()
         userinfo=config.requestUserCreation(newconfig)
-        print userinfo
+        print newconfig.getAccount().get_groups()
         self.interpreterServer.createAccounts(userinfo)
         newconfig.createWorkQ(newconfig.get_queue_name(),worklist)
         queue.listenContinouslyToQueue("reportq")
