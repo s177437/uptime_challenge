c480f33aa821ee7cc56beb45af3e66d1e0947585
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-09-15 16:27:56 +0200

    new content

--- manager/Report.py
+++ manager/Report.py
@@ -5,12 +5,12 @@ Created on 1. sep. 2015
 @author: stianstrom
 '''
 class Report():
-    queue = Queue()
     groupname=""
     course=""
 
     def createReportQueue(self, queuename,content):
-        self.queue.createQueue(queuename, content)
+        queue=Queue()
+        queue.createQueue(queuename, content)
 
     def buildReport(self, content):
         list= content.split()
@@ -18,11 +18,3 @@ class Report():
 
     def writeReportToDatabase(self,content):
         self.createReportQueue("dbreportq", content)
-
-
-
-
-
-    
-    
-    
