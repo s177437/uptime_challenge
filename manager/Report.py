ea0f3a3b0029c84080418812df4c7504c3b21c95
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-10-07 12:56:09 +0200

    new content

--- manager/Report.py
+++ manager/Report.py
@@ -1,32 +1,33 @@
 from Queue import *
 import ast
 import Pyro4
+
 '''
 Created on 1. sep. 2015
 
 @author: stianstrom
 '''
-class Report():
-    groupname=""
-    course=""
-    interpreterServer=Pyro4.Proxy("PYRONAME:interpreter")
 
-    def createReportQueue(self, queuename,content):
-        queue=Queue()
+
+class Report():
+    groupname = ""
+    course = ""
+    interpreterServer = Pyro4.Proxy("PYRONAME:interpreter")
+
+    def createReportQueue(self, queuename, content):
+        queue = Queue()
         queue.createQueue(queuename, content)
 
     def buildReport(self, content):
-	    dict = ast.literal_eval(content)
-	    self.writeReportToDatabase(dict)
-    #comment
+        dict = ast.literal_eval(content)
+        self.writeReportToDatabase(dict)
+
+    # comment
 
 
-    def writeReportToDatabase(self,reportdict):
-        groupname=reportdict['group']
-        for key,value in reportdict.iteritems() :
-            if "Award" in key :
+    def writeReportToDatabase(self, reportdict):
+        groupname = reportdict['group']
+        for key, value in reportdict.iteritems():
+            if "Award" in key:
                 self.interpreterServer.updateBalance("testaccounts", groupname, value)
         self.interpreterServer.postReportToDatabase(reportdict)
-
-
-
