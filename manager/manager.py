3e79748a8bd2958a79514e0d66c4272b59e2f800
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-10-12 21:30:14 +0200

    added functions

--- manager/manager.py
+++ manager/manager.py
@@ -21,11 +21,11 @@ class Manager():
         """
         interval = 0
         intervaltime = 900
-        time_of_day = random.randint(-12, 12)
+        time_of_day = random.uniform(-12, 12)
         while True:
             if time_of_day > 12:
                 time_of_day -= 24
-	    print time_of_day
+            print time_of_day
             config = Config()
             config.getAccount()
             newconfig = config.initDbConfig()
@@ -39,31 +39,30 @@ class Manager():
             # worklist = ["python " + path + "check_http.py db.no"]
             while (hour < 3600):
                 fetchload = newconfig.returnLoad(time_of_day)
-                currentload = int(fetchload[0])
-		
+                currentload = float(fetchload[0])
                 print currentload
-		loadincrease = int(fetchload[1])
-                time_elapsed = 0
+                loadincrease = float(fetchload[1]) / 10
+            time_elapsed = 0
+            worklist = [path + "traffic.sh " + str((currentload * 30)) + " " + str(currentload)]
+            while (time_elapsed < 900):
+                for i in grouplist:
+                    groupdict = {}
+                    groupdict.update({i: worklist})
+                    newconfig.createWorkQ(newconfig.get_queue_name(), groupdict)
+                    queue = Queue()
+                # queue.listenContinouslyToQueue("reportq")
+                timestart = time.time()
+                while (time.time() - timestart <= float(newconfig.get_interval())):
+                    print time.time() - timestart
+                    queue.receiveOneMessageFromQ("reportq", (time.time() - timestart), newconfig.get_interval())
+                if currentload + loadincrease < 0:
+                    loadincrease *= (-1)
+                currentload += loadincrease
+                print currentload
                 worklist = [path + "traffic.sh " + str((currentload * 30)) + " " + str(currentload)]
-                while (time_elapsed < 900):
-                    for i in grouplist:
-                        groupdict = {}
-                        groupdict.update({i: worklist})
-                        newconfig.createWorkQ(newconfig.get_queue_name(), groupdict)
-                        queue = Queue()
-                    # queue.listenContinouslyToQueue("reportq")
-                    timestart = time.time()
-                    while (time.time() - timestart <= float(newconfig.get_interval())):
-                        print time.time() - timestart
-                        queue.receiveOneMessageFromQ("reportq", (time.time() - timestart), newconfig.get_interval())
-                    if currentload + loadincrease < 0 :
-                        loadincrease *= (-1)
-                    currentload += loadincrease
-		    print currentload
-                    worklist = [path + "traffic.sh " + str((currentload * 30)) + " " + str(currentload)]
-                    time_elapsed += int(interval)
-                    hour += int(interval)
-                time_of_day += (1 / 96)
+                time_elapsed += float(interval)
+                hour += float(interval)
+            time_of_day += (1 / 96)
 
 
 manager = Manager()
