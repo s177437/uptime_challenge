454eb673f4b5d9a771315949d1ef6ca111617251
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-10-12 20:50:20 +0200

    new content

--- manager/manager.py
+++ manager/manager.py
@@ -19,9 +19,9 @@ class Manager():
         :return:
         :rtype:
         """
-        interval=0
-        intervaltime=900
-        time_of_day=random.randint(-12, 12)
+        interval = 0
+        intervaltime = 900
+        time_of_day = random.randint(-12, 12)
         while True:
             if time_of_day < 12:
                 time_of_day -= 24
@@ -34,33 +34,34 @@ class Manager():
             self.interpreterServer.createAccounts(userinfo)
             path = newconfig.get_script_path()
             hour = 0
-            fetchload=[]
-            #worklist = ["python " + path + "check_http.py db.no"]
-            while (hour < 3600) :
-                fetchload=newconfig.returnLoad(time_of_day)
+            fetchload = []
+            # worklist = ["python " + path + "check_http.py db.no"]
+            while (hour < 3600):
+                fetchload = newconfig.returnLoad(time_of_day)
                 currentload = int(fetchload[0])
                 loadincrease = int(fetchload[1])
-                time_elapsed = 0                
-		worklist = [path + "traffic.sh "+str((currentload*30))+" "+ str(currentload)]
-
+                time_elapsed = 0
+                worklist = [path + "traffic.sh " + str((currentload * 30)) + " " + str(currentload)]
                 while (time_elapsed < 900):
-
                     for i in grouplist:
                         groupdict = {}
                         groupdict.update({i: worklist})
                         newconfig.createWorkQ(newconfig.get_queue_name(), groupdict)
                         queue = Queue()
-			print currentload
+                        print currentload
                     # queue.listenContinouslyToQueue("reportq")
                     timestart = time.time()
                     while (time.time() - timestart <= float(newconfig.get_interval())):
                         print time.time() - timestart
                         queue.receiveOneMessageFromQ("reportq", (time.time() - timestart), newconfig.get_interval())
-			
+                    if currentload + loadincrease < 0 :
+                        loadincrease *= (-1)
                     currentload += loadincrease
-                    worklist = [path + "traffic.sh "+str((currentload*30))+" "+ str(currentload)]
+                    worklist = [path + "traffic.sh " + str((currentload * 30)) + " " + str(currentload)]
                     time_elapsed += interval
                     hour += interval
-                time_of_day+=(1/96)
+                time_of_day += (1 / 96)
+
+
 manager = Manager()
 manager.fetchConfig()
