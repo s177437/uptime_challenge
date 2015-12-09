607e5d321e0cc40f18bbc7940103c9302778352b
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-12-09 21:56:54 +0100

    first test with webuse manager

--- manager/kyrre_manager_webuse.py
+++ manager/kyrre_manager_webuse.py
@@ -19,9 +19,11 @@ class Httpmanager():
         :return:
         :rtype:
         """
+        math=WebUseMath()
+        strengthlist = math.createTimeList()
+        position=0
         while True:
             config = Config()
-            config.getAccount()
             newconfig = config.initDbConfig()
             userinfo = config.requestUserCreation(newconfig)
             grouplist = newconfig.findGroupnames(newconfig.getAccount().get_groups())
@@ -30,9 +32,20 @@ class Httpmanager():
             # worklist=[path+"traffic.sh 100 10",path+"traffic.sh 100 10"]
             # worklist=["python "+path+ "check_http.py db.no","python "+path+ "check_http.py vg.no", "python "+path+ "check_http.py facebook.com","python "+path+ "check_http.py arngren.net", "python "+path+ "check_http.py db.no","python "+path+ "check_http.py db.no"]
             # worklist = [path + "traffic.sh 100 10"]
-            worklist = [path + "webuse.pl -U 128.39.121.59 -r '10/10/10/10'"]
+            #worklist = [path + "webuse.pl -U 128.39.121.59 -r '10/10/10/10'"]
+            executable_string=path + "webuse.pl -U 128.39.121.59 -r '10/10/10/10'"
+            index=10
             for i in grouplist:
-
+                content = math.decideEntry(strengthlist,index)
+                listvalues = math.convertToList(content)
+                position=math.calculateList(listvalues)
+                index+=10
+            for i in grouplist:
+                strength_value_as_string= math.jumpToNextEntry(strengthlist, position)
+                values_in_value_string=math.convertToList(strength_value_as_string)
+                position=values_in_value_string[0]
+                strength_number= math.calculateList(values_in_value_string)
+                worklist = math.create_number_of_scripts(strength_number,executable_string)
                 groupdict = {}
                 groupdict.update({i: worklist})
                 newconfig.createWorkQ(newconfig.get_queue_name(), groupdict)
