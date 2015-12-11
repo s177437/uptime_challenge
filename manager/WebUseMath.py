cce75777602c5020d435329e6562a6585d0b472e
Stian Strøm Anderssen <s177437@stud.hioa.no>
2015-12-11 21:42:25 +0100

    Content to check position in file

--- manager/WebUseMath.py
+++ manager/WebUseMath.py
@@ -34,19 +34,14 @@ class WebUseMath :
         return listvalues
 
     def calculateList(self, listvalues):
-        position = int(listvalues[0])
         del listvalues[0]
         number = int(listvalues[0])
         calculation_algorithm = listvalues[1]
         variation = int(listvalues[2])
         if calculation_algorithm == "g":
             strength_number = int(random.gauss(number, variation))
-            #print "Algorithm:", calculation_algorithm, "Strength number:", strength_number
         elif calculation_algorithm == "r":
             strength_number = int(random.uniform((number - variation), (number + variation)))
-            #print "Algorithm:", calculation_algorithm, "Strength number:", strength_number
-        #worklist = self.create_number_of_scripts(strength_number)
-        #print "algorithm:", calculation_algorithm, worklist,  "length:", len(worklist), strength_number
         return strength_number
 
     def create_number_of_scripts(self, number_of_scripts_to_make, executable_string) :
