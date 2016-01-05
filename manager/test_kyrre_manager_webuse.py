990879ba3403f0dcd4868d97f2232222ad72d594
Stian Strom Anderssen <s177437@stud.hioa.no>
2016-01-05 13:23:16 +0000

    manager test

---
+++ manager/test_kyrre_manager_webuse.py
@@ -0,0 +1,77 @@
+from Queue import *
+from Config import *
+from WebUseMath import *
+import pika
+import couchdb
+import Pyro4
+import time
+
+
+class Httpmanager():
+    """
+    This module is the main class for the manager project. This module takes care of the execution of the functions.
+    """
+    interpreterServer = Pyro4.Proxy("PYRONAME:interpreter")
+    # comment
+    def fetchConfig(self):
+        """
+        This function is the main method where everything is governed on the manager.
+        :return:
+        :rtype:
+        """	
+        math=WebUseMath()
+        strengthlist = math.createTimeList()
+        position=0
+        config = Config()
+        newconfig = config.initDbConfig()
+        userinfo = config.requestUserCreation(newconfig)
+        grouplist = newconfig.findGroupnames(newconfig.getAccount().get_groups())
+        self.interpreterServer.createAccounts(userinfo)
+        path = newconfig.get_script_path()
+        #executable_string=path + "webuse.pl -U 128.39.121.59 -r '10:10:10:10'"
+        #executable_string=path + "webuse.pl -U 128.39.121.59 -r '10/10/10/10'"
+        index=0
+        print "Interval",newconfig.get_interval()
+        positiondict = {}
+        for i in grouplist:
+            userconfig = self.interpreterServer.getFileAndOffsetFromUser(i)
+	    ipconfig = self.interpreterServer.getIpFromUser(i)
+            ip=ipconfig["ipaddress"]
+            executable_string=path + "webuse.pl -U " + ip +" -r '10:10:10:10'"
+            print userconfig
+            index = int(userconfig["offset"])
+            print "INDEX", index
+            content = math.decideEntry(strengthlist,index)
+            worklist=[]
+            listvalues = math.convertToList(content)
+            position= int(listvalues[0])
+            print "USEREN:", i,"POSITION", position
+            strength_number=math.calculateList(listvalues)
+            worklist = math.create_number_of_scripts(strength_number,executable_string)
+            groupdict = {}
+            groupdict.update({i: worklist})
+            newconfig.createWorkQ(newconfig.get_queue_name(), groupdict)
+            worklist = []
+            positiondict.update({i:position})
+        while True:
+            for i, position in positiondict.iteritems():
+                print "USER:", i,"POSITION", position
+                strength_value_as_string= math.jumpToNextEntry(strengthlist, int(position))
+                values_in_value_string=math.convertToList(strength_value_as_string)
+                #position=int(values_in_value_string[0])
+                strength_number = math.calculateList(values_in_value_string)
+                worklist = math.create_number_of_scripts(strength_number,executable_string)
+                groupdict = {}
+                groupdict.update({i: worklist})
+                if position == 288 :
+                    positiondict[i]=0
+                else :
+                    positiondict[i]=position+1
+                newconfig.createWorkQ(newconfig.get_queue_name(), groupdict)
+                queue = Queue()
+            # queue.listenContinouslyToQueue("reportq")
+            queue.receiveOneMessageFromQ("reportq", newconfig.get_interval())
+
+
+manager = Httpmanager()
+manager.fetchConfig()
