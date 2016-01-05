a3dfc1000a1a52e39fe0a441d382c43f52ce9ce9
Stian Strøm Anderssen <s177437@stud.hioa.no>
2016-01-05 13:51:13 +0100

    new content

--- worker/worker_kyrre.py
+++ worker/worker_kyrre.py
@@ -20,29 +20,27 @@ class worker():
 
         # do :
         # sloop=False
-        credentials = pika.PlainCredentials('guest', 'guest')
-        connection = pika.BlockingConnection(pika.ConnectionParameters('10.1.1.175', 5672, '/', credentials))
-        channel = connection.channel()
-        channel.queue_declare(queue='testq')
-        try:
-            method_frame, header_frame, body = channel.basic_get(queue='testq')
-            if method_frame.NAME == 'Basic.GetEmpty':
-                #print "You end up here"
-                connection.close()
-            else:
-                # loop=True
-                channel.basic_ack(delivery_tag=method_frame.delivery_tag)
-                print "Received job:", body, "starting job to reply"
-                self.replyToMaster(body)
-                connection.close()
-		time.sleep(2)
-                self.fetchJobFromQ()
-        # while loop = True
-        except AttributeError:
-            print "No content"
-	    connection.close()
-            time.sleep(2)
-            self.fetchJobFromQ()
+        while 1:
+                credentials = pika.PlainCredentials('guest', 'guest')
+                connection = pika.BlockingConnection(pika.ConnectionParameters('10.1.1.175', 5672, '/', credentials))
+                channel = connection.channel()
+                channel.queue_declare(queue='testq')
+                try:
+                    method_frame, header_frame, body = channel.basic_get(queue='testq')
+                    if method_frame.NAME == 'Basic.GetEmpty':
+                        connection.close()
+                    else:
+                        channel.basic_ack(delivery_tag=method_frame.delivery_tag)
+                        print "Received job:", body, "starting job to reply"
+                        self.replyToMaster(body)
+                        connection.close()
+                        time.sleep(2)
+                except AttributeError:
+                    print "No content"
+                    connection.close()
+                    time.sleep(2)
+
+
 
     def doJob(self, command):
         outputdata = self.getcommandoutput(command)
