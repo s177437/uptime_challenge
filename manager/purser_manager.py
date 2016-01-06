b6165274f1788f7492b4004373b7db8b936ff630
Stian Strøm Anderssen <s177437@stud.hioa.no>
2016-01-06 20:23:31 +0100

    purser_manager added for testing

---
+++ manager/purser_manager.py
@@ -0,0 +1,46 @@
+from Queue import *
+from Config import *
+from WebUseMath import *
+import pika
+import couchdb
+import Pyro4
+import time
+
+
+class Httpmanager():
+    """
+    This module is the main class for the manager project. This module takes care of the execution of the functions.
+    """
+    interpreterServer = Pyro4.Proxy("PYRONAME:interpreter")
+    # comment
+    def fetchConfig(self):
+        """
+        This function is the main method where everything is governed on the manager.
+        :return:
+        :rtype:
+        """
+        math=WebUseMath()
+        config = Config()
+        newconfig = config.initDbConfig()
+        userinfo = config.requestUserCreation(newconfig)
+        grouplist = newconfig.findGroupnames(newconfig.getAccount().get_groups())
+        self.interpreterServer.createAccounts(userinfo)
+        path = newconfig.get_script_path()
+        print "Interval",newconfig.get_interval()
+        positiondict = {}
+        while True:
+            for i in grouplist:
+                userconfig = self.interpreterServer.getUserConfig(i,"couchdb")
+                ip=userconfig["ipaddress"]
+                worklist=[]
+                worklist = [{"ip":ip,"sentance":userconfig["sentance"],"filepath":userconfig["filepath"],"file":userconfig["file"],"timestamp":time.time()}]
+                groupdict = {}
+                groupdict.update({i: worklist})
+                newconfig.createWorkQ(newconfig.get_queue_name(), groupdict)
+                worklist = []
+                queue = Queue()
+                queue.receiveOneMessageFromQ("purser_report_q", newconfig.get_interval())
+
+
+manager = Httpmanager()
+manager.fetchConfig()
